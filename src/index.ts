9f4c8e5df90af77969d587387c394d984673122d
Joao <joaojacomedev@gmail.com>
2024-10-15 15:45:56 -0500

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import { connectDB } from './config/db';
 import userRoutes from './routes/auth.route';
 
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-dotenv.config()
+
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-app.use('/api', userRoutes);
+app.use('/api/auth', userRoutes);
 
 app.listen( PORT, async () => {
     console.log('Server running on port: ', PORT)
