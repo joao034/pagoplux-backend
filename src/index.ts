56bbd06dbb68ce72c112d5ec1c6f72eb90e7e632
Joao <joaojacomedev@gmail.com>
2024-10-16 23:57:56 -0500

    delete unused code

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use( '/api/auth', userRoutes );
-app.use( '/api/transaction', transactionRoutes )
+app.use( '/api/transactions', transactionRoutes )
 
 app.listen( PORT, async () => {
     console.log('Server running on port: ', PORT)
